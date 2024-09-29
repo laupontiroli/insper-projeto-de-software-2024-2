dc14c1061e44af344d85851d9ae18feae1922ad4
Eduardo Santana <ezambomsantana@gmail.com>
2024-09-29 17:09:02 -0300

    add aula 12

---
+++ aula-12/campeonato/src/main/java/br/insper/loja/common/KafkaProducerConfig.java
@@ -0,0 +1,35 @@
+package br.insper.loja.common;
+
+import br.insper.loja.partida.dto.RetornarPartidaDTO;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, RetornarPartidaDTO> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, RetornarPartidaDTO> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
